d3b1fa27a3e53d90b293b80a70e292ee3160222d
Charlotte <charlotte.nicaudie@insa-rennes.fr>
2021-11-21 17:23:56 +0000

    Added different time base options

--- graphics_coursework/Assets/Scripts/LightManager.cs
+++ graphics_coursework/Assets/Scripts/LightManager.cs
@@ -9,25 +9,41 @@ public class LightManager : MonoBehaviour
     public Gradient fogColor;
     public AnimationCurve intensityCurve;
 
-    private Light m_sunLight;
+    public enum TimeBase { HOURS, MINUTES, SECONDS }
+    public TimeBase timeBase;
 
+    private Light m_sunLight;
     private const float k_maxIntensity = 0.7f;
     private const float k_maxHoursInDay = 24f;
 
     [SerializeField, Range(0, k_maxHoursInDay)] private float m_timeOfDay;
+    [SerializeField] private float m_timeSpeed;
 
     // ==================================
 
     private void Start()
     {
         m_sunLight = GetComponent<Light>();
+
+        switch (timeBase)
+        {
+            case TimeBase.HOURS:
+                m_timeSpeed = 3600;
+                break;
+            case TimeBase.MINUTES:
+                m_timeSpeed = 60;
+                break;
+            case TimeBase.SECONDS:
+                m_timeSpeed = 1;
+                break;
+        }
     }
 
     private void Update()
     {
         if (Application.isPlaying)
         {
-            m_timeOfDay += Time.deltaTime;
+            m_timeOfDay += 1 / m_timeSpeed * Time.deltaTime;
             m_timeOfDay %= k_maxHoursInDay;
         }
 
